62d9746cd1042e34fd464b4f543f1b4d4b0b5135
AYUSH <ayushpatel400004@gmail.com>
2024-04-04 13:18:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Web3 from './pages/Web3'
+import Fullstack from './pages/Fullstack'
+import AIPage from './pages/AIPage'
+
+
+function App() {
+  
+
+  return (
+    <div>
+       <BrowserRouter>
+            <Routes>
+                <Route element={<Dashboard></Dashboard>} path='/'></Route>
+                <Route element={<Web3></Web3>} path='/Web3'></Route>
+                <Route element={<Fullstack></Fullstack>} path='/Fullstack'></Route>
+                <Route element={<AIPage></AIPage>} path='/AI'></Route>
+            </Routes>
+       </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+export default function(){
+    return <div className="text-5xl">
+    Dashboard
+    </div>
+}
